9e3c6aea6ba1001c2f1a4a3a59509ce48d9e2540
Tonny <anthonykioko@gmail.com>
2022-11-17 11:27:25 -0800

    1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *bt = NULL;
+
+	if (!parent)
+		return (NULL);
+	bt = calloc(1, sizeof(binary_tree_t));
+	if (!bt)
+		return (NULL);
+	bt->n = value;
+	bt->parent = parent;
+	if (parent->left)
+	{
+		bt->left = parent->left;
+		bt->left->parent = bt;
+	}
+	parent->left = bt;
+	return (bt);
+}
